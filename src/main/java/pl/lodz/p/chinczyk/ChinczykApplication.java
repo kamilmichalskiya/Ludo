cc0c3f5a3cab5364fe7c4d393c770c8eb0dde581
kamilmichalskiya <kamilmichalskiya@users.noreply.github.com>
2021-12-01 18:37:57 +0100

    Initial commit

---
+++ src/main/java/pl/lodz/p/chinczyk/ChinczykApplication.java
@@ -0,0 +1,13 @@
+package pl.lodz.p.chinczyk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChinczykApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChinczykApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/lodz/p/chinczyk/ChinczykApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.lodz.p.chinczyk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChinczykApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
