0e6d80986dfa381060d1611d011c186e7985c060
Oxozon <oxozon.o4@gmail.com>
2022-02-23 20:27:01 +0100

    add player names placeholders

--- src/main/FE/src/components/organisms/GameBoard/GameBoard.js
+++ src/main/FE/src/components/organisms/GameBoard/GameBoard.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import Box from 'components/atoms/Box/Box';
+import { Label } from 'components/atoms/Label/Label';
 import { PrimaryButton } from 'components/atoms/Button/Button';
-import { Wrapper, StyledRow, StyledNavigation } from './GameBoard.styles';
+import { Wrapper, StyledRow, StyledNavigation, StyledGameBoardContainer, HeaderSection } from './GameBoard.styles';
 import { Link } from 'react-router-dom';
 import Dice from 'react-dice-roll';
 
@@ -64,168 +65,182 @@ const GamesBoard = ({ location: { state } }) => {
     console.log('Start Game!');
   };
 
+  const rollDice = () => {
+    console.log('Roll Dice!');
+  };
+
   console.log('GameBoard initialize params: ', state);
 
   return (
     <>
       <Wrapper>
         <h1>{isLoading ? 'Loading...' : ''}</h1>
-        <div>
-          
-        </div>
         <StyledNavigation>
           <PrimaryButton onClick={startGame}>Start Game!</PrimaryButton>
-          <PrimaryButton onClick={startGame}>Roll Dice!</PrimaryButton>
+          <PrimaryButton onClick={rollDice}>Roll Dice!</PrimaryButton>
           <Link to="/">
             <PrimaryButton>Leave Game :c</PrimaryButton>
           </Link>
         </StyledNavigation>
-        <br /> <br />
-        <StyledRow>
-          <Box type="N" color="yellow" id="Y_BASE_1" pawnsArray={pawnsObject} />
-          <Box type="N" color="yellow" id="Y_BASE_2" pawnsArray={pawnsObject} />
-          <Box type="N" visibility="hidden" />
-          <Box type="N" visibility="hidden" />
-          <Box type="N" color="white" id="Y_8" pawnsArray={pawnsObject} />
-          <Box type="N" color="white" id="Y_9" pawnsArray={pawnsObject} />
-          <Box type="N" color="red" id="R_0" pawnsArray={pawnsObject} />
-          <Box type="N" visibility="hidden" />
-          <Box type="N" visibility="hidden" />
-          <Box type="N" color="red" id="R_BASE_1" pawnsArray={pawnsObject} />
-          <Box type="N" color="red" id="R_BASE_2" pawnsArray={pawnsObject} />
-        </StyledRow>
-        <StyledRow>
-          <Box type="N" color="yellow" id="Y_BASE_3" pawnsArray={pawnsObject} />
-          <Box type="N" color="yellow" id="Y_BASE_4" pawnsArray={pawnsObject} />
-          <Box type="N" visibility="hidden" />
-          <Box type="N" visibility="hidden" />
-          <Box type="N" color="white" id="Y_7" pawnsArray={pawnsObject} />
-          <Box type="N" color="red" id="R_HOME" pawnsArray={pawnsObject} />
-          <Box type="N" color="white" id="R_1" pawnsArray={pawnsObject} />
-          <Box type="N" visibility="hidden" />
-          <Box type="N" visibility="hidden" />
-          <Box type="N" color="red" id="R_BASE_3" pawnsArray={pawnsObject} />
-          <Box type="N" color="red" id="R_BASE_4" pawnsArray={pawnsObject} />
-        </StyledRow>
-        <StyledRow>
-          <Box type="N" visibility="hidden" />
-          <Box type="N" visibility="hidden" />
-          <Box type="N" visibility="hidden" />
-          <Box type="N" visibility="hidden" />
-          <Box type="N" color="white" id="Y_6" pawnsArray={pawnsObject} />
-          <Box type="N" color="red" id="R_HOME" pawnsArray={pawnsObject} />
-          <Box type="N" color="white" id="R_2" pawnsArray={pawnsObject} />
-          <Box type="N" visibility="hidden" />
-          <Box type="N" visibility="hidden" />
-          <Box type="N" visibility="hidden" />
-          <Box type="N" visibility="hidden" />
-        </StyledRow>
-        <StyledRow>
-          <Box type="N" visibility="hidden" />
-          <Box type="N" visibility="hidden" />
-          <Box type="N" visibility="hidden" />
-          <Box type="N" visibility="hidden" />
-          <Box type="N" color="white" id="Y_5" pawnsArray={pawnsObject} />
-          <Box type="N" color="red" id="R_HOME" pawnsArray={pawnsObject} />
-          <Box type="N" color="white" id="R_3" pawnsArray={pawnsObject} />
-          <Box type="N" visibility="hidden" />
-          <Box type="N" visibility="hidden" />
-          <Box type="N" visibility="hidden" />
-          <Box type="N" visibility="hidden" />
-        </StyledRow>
-        {/* Middle Section */}
-        <StyledRow>
-          <Box type="N" color="yellow" id="Y_0" pawnsArray={pawnsObject} />
-          <Box type="N" color="white" id="Y_1" pawnsArray={pawnsObject} />
-          <Box type="N" color="white" id="Y_2" pawnsArray={pawnsObject} />
-          <Box type="N" color="white" id="Y_3" pawnsArray={pawnsObject} />
-          <Box type="N" color="white" id="Y_4" pawnsArray={pawnsObject} />
-          <Box type="N" color="red" id="R_HOME" pawnsArray={pawnsObject} />
-          <Box type="N" color="white" id="R_4" pawnsArray={pawnsObject} />
-          <Box type="N" color="white" id="R_5" pawnsArray={pawnsObject} />
-          <Box type="N" color="white" id="R_6" pawnsArray={pawnsObject} />
-          <Box type="N" color="white" id="R_7" pawnsArray={pawnsObject} />
-          <Box type="N" color="white" id="R_8" pawnsArray={pawnsObject} />
-        </StyledRow>
-        <StyledRow>
-          <Box type="N" color="white" id="B_9" pawnsArray={pawnsObject} />
-          <Box type="N" color="yellow" id="Y_HOME" pawnsArray={pawnsObject} />
-          <Box type="N" color="yellow" id="Y_HOME" pawnsArray={pawnsObject} />
-          <Box type="N" color="yellow" id="Y_HOME" pawnsArray={pawnsObject} />
-          <Box type="N" color="yellow" id="Y_HOME" pawnsArray={pawnsObject} />
-          <Dice size="50" placement="top-right" style={{ margin: '50px' }}></Dice>
-          <Box type="N" color="green" id="G_HOME" pawnsArray={pawnsObject} />
-          <Box type="N" color="green" id="G_HOME" pawnsArray={pawnsObject} />
-          <Box type="N" color="green" id="G_HOME" pawnsArray={pawnsObject} />
-          <Box type="N" color="green" id="G_HOME" pawnsArray={pawnsObject} />
-          <Box type="N" color="white" id="R_9" pawnsArray={pawnsObject} />
-        </StyledRow>
-        <StyledRow>
-          <Box type="N" color="white" id="B_8" pawnsArray={pawnsObject} />
-          <Box type="N" color="white" id="B_7" pawnsArray={pawnsObject} />
-          <Box type="N" color="white" id="B_6" pawnsArray={pawnsObject} />
-          <Box type="N" color="white" id="B_5" pawnsArray={pawnsObject} />
-          <Box type="N" color="white" id="B_4" pawnsArray={pawnsObject} />
-          <Box type="N" color="blue" id="B_HOME" pawnsArray={pawnsObject} />
-          <Box type="N" color="white" id="G_4" pawnsArray={pawnsObject} />
-          <Box type="N" color="white" id="G_3" pawnsArray={pawnsObject} />
-          <Box type="N" color="white" id="G_2" pawnsArray={pawnsObject} />
-          <Box type="N" color="white" id="G_1" pawnsArray={pawnsObject} />
-          <Box type="N" color="green" id="G_0" pawnsArray={pawnsObject} />
-        </StyledRow>
-        {/* Lower Section */}
-        <StyledRow>
-          <Box type="N" visibility="hidden" />
-          <Box type="N" visibility="hidden" />
-          <Box type="N" visibility="hidden" />
-          <Box type="N" visibility="hidden" />
-          <Box type="N" color="white" id="B_3" pawnsArray={pawnsObject} />
-          <Box type="N" color="blue" id="B_HOME" pawnsArray={pawnsObject} />
-          <Box type="N" color="white" id="G_5" pawnsArray={pawnsObject} />
-          <Box type="N" visibility="hidden" />
-          <Box type="N" visibility="hidden" />
-          <Box type="N" visibility="hidden" />
-          <Box type="N" visibility="hidden" />
-        </StyledRow>
-        <StyledRow>
-          <Box type="N" visibility="hidden" />
-          <Box type="N" visibility="hidden" />
-          <Box type="N" visibility="hidden" />
-          <Box type="N" visibility="hidden" />
-          <Box type="N" color="white" id="B_2" pawnsArray={pawnsObject} />
-          <Box type="N" color="blue" id="B_HOME" pawnsArray={pawnsObject} />
-          <Box type="N" color="white" id="G_6" pawnsArray={pawnsObject} />
-          <Box type="N" visibility="hidden" />
-          <Box type="N" visibility="hidden" />
-          <Box type="N" visibility="hidden" />
-          <Box type="N" visibility="hidden" />
-        </StyledRow>
-        <StyledRow>
-          <Box type="N" color="blue" id="B_BASE_1" pawnsArray={pawnsObject} />
-          <Box type="N" color="blue" id="B_BASE_2" pawnsArray={pawnsObject} />
-          <Box type="N" visibility="hidden" />
-          <Box type="N" visibility="hidden" />
-          <Box type="N" color="white" id="B_1" pawnsArray={pawnsObject} />
-          <Box type="N" color="blue" id="B_HOME" pawnsArray={pawnsObject} />
-          <Box type="N" color="white" id="G_7" pawnsArray={pawnsObject} />
-          <Box type="N" visibility="hidden" />
-          <Box type="N" visibility="hidden" />
-          <Box type="N" color="green" id="G_BASE_1" pawnsArray={pawnsObject} />
-          <Box type="N" color="green" id="G_BASE_2" pawnsArray={pawnsObject} />
-        </StyledRow>
-        <StyledRow>
-          <Box type="N" color="blue" id="B_BASE_3" pawnsArray={pawnsObject} />
-          <Box type="N" color="blue" id="B_BASE_4" pawnsArray={pawnsObject} />
-          <Box type="N" visibility="hidden" />
-          <Box type="N" visibility="hidden" />
-          <Box type="N" color="blue" id="B_0" pawnsArray={pawnsObject} />
-          <Box type="N" color="white" id="G_8" pawnsArray={pawnsObject} />
-          <Box type="N" color="white" id="G_9" pawnsArray={pawnsObject} />
-          <Box type="N" visibility="hidden" />
-          <Box type="N" visibility="hidden" />
-          <Box type="N" color="green" id="G_BASE_3" pawnsArray={pawnsObject} />
-          <Box type="N" color="green" id="G_BASE_4" pawnsArray={pawnsObject} />
-        </StyledRow>
+        <StyledGameBoardContainer>
+          <StyledRow>
+            <HeaderSection>
+              <Label>Player 1</Label>
+              <Label>Player 2</Label>
+            </HeaderSection>
+          </StyledRow>
+          <StyledRow>
+            <Box type="N" color="yellow" id="Y_BASE_1" pawnsArray={pawnsObject} />
+            <Box type="N" color="yellow" id="Y_BASE_2" pawnsArray={pawnsObject} />
+            <Box type="N" visibility="hidden" />
+            <Box type="N" visibility="hidden" />
+            <Box type="N" color="white" id="Y_8" pawnsArray={pawnsObject} />
+            <Box type="N" color="white" id="Y_9" pawnsArray={pawnsObject} />
+            <Box type="N" color="red" id="R_0" pawnsArray={pawnsObject} />
+            <Box type="N" visibility="hidden" />
+            <Box type="N" visibility="hidden" />
+            <Box type="N" color="red" id="R_BASE_1" pawnsArray={pawnsObject} />
+            <Box type="N" color="red" id="R_BASE_2" pawnsArray={pawnsObject} />
+          </StyledRow>
+          <StyledRow>
+            <Box type="N" color="yellow" id="Y_BASE_3" pawnsArray={pawnsObject} />
+            <Box type="N" color="yellow" id="Y_BASE_4" pawnsArray={pawnsObject} />
+            <Box type="N" visibility="hidden" />
+            <Box type="N" visibility="hidden" />
+            <Box type="N" color="white" id="Y_7" pawnsArray={pawnsObject} />
+            <Box type="N" color="red" id="R_HOME" pawnsArray={pawnsObject} />
+            <Box type="N" color="white" id="R_1" pawnsArray={pawnsObject} />
+            <Box type="N" visibility="hidden" />
+            <Box type="N" visibility="hidden" />
+            <Box type="N" color="red" id="R_BASE_3" pawnsArray={pawnsObject} />
+            <Box type="N" color="red" id="R_BASE_4" pawnsArray={pawnsObject} />
+          </StyledRow>
+          <StyledRow>
+            <Box type="N" visibility="hidden" />
+            <Box type="N" visibility="hidden" />
+            <Box type="N" visibility="hidden" />
+            <Box type="N" visibility="hidden" />
+            <Box type="N" color="white" id="Y_6" pawnsArray={pawnsObject} />
+            <Box type="N" color="red" id="R_HOME" pawnsArray={pawnsObject} />
+            <Box type="N" color="white" id="R_2" pawnsArray={pawnsObject} />
+            <Box type="N" visibility="hidden" />
+            <Box type="N" visibility="hidden" />
+            <Box type="N" visibility="hidden" />
+            <Box type="N" visibility="hidden" />
+          </StyledRow>
+          <StyledRow>
+            <Box type="N" visibility="hidden" />
+            <Box type="N" visibility="hidden" />
+            <Box type="N" visibility="hidden" />
+            <Box type="N" visibility="hidden" />
+            <Box type="N" color="white" id="Y_5" pawnsArray={pawnsObject} />
+            <Box type="N" color="red" id="R_HOME" pawnsArray={pawnsObject} />
+            <Box type="N" color="white" id="R_3" pawnsArray={pawnsObject} />
+            <Box type="N" visibility="hidden" />
+            <Box type="N" visibility="hidden" />
+            <Box type="N" visibility="hidden" />
+            <Box type="N" visibility="hidden" />
+          </StyledRow>
+          {/* Middle Section */}
+          <StyledRow>
+            <Box type="N" color="yellow" id="Y_0" pawnsArray={pawnsObject} />
+            <Box type="N" color="white" id="Y_1" pawnsArray={pawnsObject} />
+            <Box type="N" color="white" id="Y_2" pawnsArray={pawnsObject} />
+            <Box type="N" color="white" id="Y_3" pawnsArray={pawnsObject} />
+            <Box type="N" color="white" id="Y_4" pawnsArray={pawnsObject} />
+            <Box type="N" color="red" id="R_HOME" pawnsArray={pawnsObject} />
+            <Box type="N" color="white" id="R_4" pawnsArray={pawnsObject} />
+            <Box type="N" color="white" id="R_5" pawnsArray={pawnsObject} />
+            <Box type="N" color="white" id="R_6" pawnsArray={pawnsObject} />
+            <Box type="N" color="white" id="R_7" pawnsArray={pawnsObject} />
+            <Box type="N" color="white" id="R_8" pawnsArray={pawnsObject} />
+          </StyledRow>
+          <StyledRow>
+            <Box type="N" color="white" id="B_9" pawnsArray={pawnsObject} />
+            <Box type="N" color="yellow" id="Y_HOME" pawnsArray={pawnsObject} />
+            <Box type="N" color="yellow" id="Y_HOME" pawnsArray={pawnsObject} />
+            <Box type="N" color="yellow" id="Y_HOME" pawnsArray={pawnsObject} />
+            <Box type="N" color="yellow" id="Y_HOME" pawnsArray={pawnsObject} />
+            <Dice size="50" placement="top-right" disabled={true}></Dice>
+            <Box type="N" color="green" id="G_HOME" pawnsArray={pawnsObject} />
+            <Box type="N" color="green" id="G_HOME" pawnsArray={pawnsObject} />
+            <Box type="N" color="green" id="G_HOME" pawnsArray={pawnsObject} />
+            <Box type="N" color="green" id="G_HOME" pawnsArray={pawnsObject} />
+            <Box type="N" color="white" id="R_9" pawnsArray={pawnsObject} />
+          </StyledRow>
+          <StyledRow>
+            <Box type="N" color="white" id="B_8" pawnsArray={pawnsObject} />
+            <Box type="N" color="white" id="B_7" pawnsArray={pawnsObject} />
+            <Box type="N" color="white" id="B_6" pawnsArray={pawnsObject} />
+            <Box type="N" color="white" id="B_5" pawnsArray={pawnsObject} />
+            <Box type="N" color="white" id="B_4" pawnsArray={pawnsObject} />
+            <Box type="N" color="blue" id="B_HOME" pawnsArray={pawnsObject} />
+            <Box type="N" color="white" id="G_4" pawnsArray={pawnsObject} />
+            <Box type="N" color="white" id="G_3" pawnsArray={pawnsObject} />
+            <Box type="N" color="white" id="G_2" pawnsArray={pawnsObject} />
+            <Box type="N" color="white" id="G_1" pawnsArray={pawnsObject} />
+            <Box type="N" color="green" id="G_0" pawnsArray={pawnsObject} />
+          </StyledRow>
+          {/* Lower Section */}
+          <StyledRow>
+            <Box type="N" visibility="hidden" />
+            <Box type="N" visibility="hidden" />
+            <Box type="N" visibility="hidden" />
+            <Box type="N" visibility="hidden" />
+            <Box type="N" color="white" id="B_3" pawnsArray={pawnsObject} />
+            <Box type="N" color="blue" id="B_HOME" pawnsArray={pawnsObject} />
+            <Box type="N" color="white" id="G_5" pawnsArray={pawnsObject} />
+            <Box type="N" visibility="hidden" />
+            <Box type="N" visibility="hidden" />
+            <Box type="N" visibility="hidden" />
+            <Box type="N" visibility="hidden" />
+          </StyledRow>
+          <StyledRow>
+            <Box type="N" visibility="hidden" />
+            <Box type="N" visibility="hidden" />
+            <Box type="N" visibility="hidden" />
+            <Box type="N" visibility="hidden" />
+            <Box type="N" color="white" id="B_2" pawnsArray={pawnsObject} />
+            <Box type="N" color="blue" id="B_HOME" pawnsArray={pawnsObject} />
+            <Box type="N" color="white" id="G_6" pawnsArray={pawnsObject} />
+            <Box type="N" visibility="hidden" />
+            <Box type="N" visibility="hidden" />
+            <Box type="N" visibility="hidden" />
+            <Box type="N" visibility="hidden" />
+          </StyledRow>
+          <StyledRow>
+            <Box type="N" color="blue" id="B_BASE_1" pawnsArray={pawnsObject} />
+            <Box type="N" color="blue" id="B_BASE_2" pawnsArray={pawnsObject} />
+            <Box type="N" visibility="hidden" />
+            <Box type="N" visibility="hidden" />
+            <Box type="N" color="white" id="B_1" pawnsArray={pawnsObject} />
+            <Box type="N" color="blue" id="B_HOME" pawnsArray={pawnsObject} />
+            <Box type="N" color="white" id="G_7" pawnsArray={pawnsObject} />
+            <Box type="N" visibility="hidden" />
+            <Box type="N" visibility="hidden" />
+            <Box type="N" color="green" id="G_BASE_1" pawnsArray={pawnsObject} />
+            <Box type="N" color="green" id="G_BASE_2" pawnsArray={pawnsObject} />
+          </StyledRow>
+          <StyledRow>
+            <Box type="N" color="blue" id="B_BASE_3" pawnsArray={pawnsObject} />
+            <Box type="N" color="blue" id="B_BASE_4" pawnsArray={pawnsObject} />
+            <Box type="N" visibility="hidden" />
+            <Box type="N" visibility="hidden" />
+            <Box type="N" color="blue" id="B_0" pawnsArray={pawnsObject} />
+            <Box type="N" color="white" id="G_8" pawnsArray={pawnsObject} />
+            <Box type="N" color="white" id="G_9" pawnsArray={pawnsObject} />
+            <Box type="N" visibility="hidden" />
+            <Box type="N" visibility="hidden" />
+            <Box type="N" color="green" id="G_BASE_3" pawnsArray={pawnsObject} />
+            <Box type="N" color="green" id="G_BASE_4" pawnsArray={pawnsObject} />
+          </StyledRow>
+          <StyledRow>
+            <HeaderSection>
+              <Label>Player 3</Label>
+              <Label>Player 4</Label>
+            </HeaderSection>
+          </StyledRow>
+        </StyledGameBoardContainer>
       </Wrapper>
     </>
   );
